56f98ee5c08a071649f8d1030229cf2ab94e6540
fitsvo <fits.v@mail.ru>
2021-03-23 23:47:05 +1000

    Add first part cloud storage

---
+++ service/src/main/AuthCommand.java
@@ -0,0 +1,17 @@
+package main;
+
+public class AuthCommand
+        extends DataPackage
+{
+
+    public final String login;
+    public final String password;
+
+
+    public AuthCommand(String login, String password)
+    {
+        this.login = login;
+        this.password = password;
+    }
+
+}
